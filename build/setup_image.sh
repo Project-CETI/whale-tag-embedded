f47a285bd748957d7d2e8afb5f5bee2bf687de15
Peter Malkin <github.com/PeterMalkin>
2021-03-27 15:25:41 -0700

    Updated the setup_image.sh to follow Daniel's setup steps.
    Install packages for all IMUs I found in the CommandsToConfig script
    Install OctoBoard AudioInjector software
    Remove the filesystem logging, added script to remount readonly.
    Make pi log less enables to run off the root partition mounted ro
    (see https://medium.com/swlh/make-your-raspberry-pi-file-system-read-only-raspbian-buster-c558694de79)
    Install PiSugar software servers, however there seems to be an issue with them
    Filed https://github.com/PiSugar/pisugar-power-manager-rs/issues/23
    Set pi's password to ceticeti
    Create default wireless network to connect to (ssid:ceti, password:ceticeti)

--- build/setup_image.sh
+++ build/setup_image.sh
@@ -48,8 +48,9 @@ apt update
 
 time apt install ${APT_NONINTERACTIVE} --fix-broken --no-upgrade \
   alsa-utils \
-  avahi-utils \
   aiy-usb-gadget \
+  avahi-utils \
+  dkms \
   dnsmasq \
   flac \
   i2c-tools \
@@ -59,6 +60,8 @@ time apt install ${APT_NONINTERACTIVE} --fix-broken --no-upgrade \
   python3-pip \
   python3-bluez \
   python3-dbus \
+  netcat \
+
 
 apt ${APT_NONINTERACTIVE} autoremove
 
@@ -67,22 +70,60 @@ pip3 install --retries 10 --default-timeout=60 \
                   -r /dev/stdin <<EOF
 cachetools==4.1.1
 sparkfun-qwiic-icm20948
+adafruit-circuitpython-bno08x
+adafruit-circuitpython-ads1x15
 EOF
 
-# Install general packages.
-install_package $(ls ${OUT_DIR}/ceti-tag-set-hostname_*_all.deb)
-install_package $(ls ${OUT_DIR}/ceti-tag-data-capture_*_all.deb)
+# Enable I2C
+sudo raspi-config nonint do_i2c 0
 
 # Enable SSH.
 touch /boot/ssh
 
-# Update boot configuration (/boot/config.txt).
-sed -i -e "s/^dtparam=audio=on/#\0/" /boot/config.txt
-sed -i -e "s/^dtparam=spi=on/#\0/" /boot/config.txt
-sed -i -e "s/^dtparam=i2c_arm=on/#\0/" /boot/config.txt
-
 # Update keyboard layout.
 sed -i -e 's/"gb"/"us"/' /etc/default/keyboard
+
+# Change default password
+echo -e "ceticeti\nceticeti" | passwd pi
+
+# Install octoboard audio injector
+apt remove pulseaudio
+TEMP_DEB="$(mktemp)".deb
+wget -O "$TEMP_DEB" 'https://github.com/Audio-Injector/stereo-and-zero/raw/master/audio.injector.scripts_0.1-1_all.deb'
+dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB"
+audioInjector-setup.sh
+
+# Install PiSugar
+TEMP_DEB="$(mktemp)".deb
+wget -O "$TEMP_DEB" 'http://cdn.pisugar.com/release/pisugar-server_1.4.9_armhf.deb'
+dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB"
+
+TEMP_DEB="$(mktemp)".deb
+wget -O "$TEMP_DEB" 'http://cdn.pisugar.com/release/pisugar-poweroff_1.4.9_armhf.deb'
+dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB"
+
+# Minimize logging
+# See article: https://medium.com/swlh/make-your-raspberry-pi-file-system-read-only-raspbian-buster-c558694de79
+apt remove --purge -y triggerhappy logrotate dphys-swapfile
+apt autoremove --purge -y
+apt install -y busybox-syslogd
+apt remove --purge -y rsyslog
+rm -rf /var/lib/dhcp /var/lib/dhcpcd5 /var/spool /etc/resolv.conf
+ln -s /tmp /var/lib/dhcp
+ln -s /tmp /var/lib/dhcpcd5
+ln -s /tmp /var/spool
+touch /tmp/dhcpcd.resolv.conf
+ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf
+
+# Create /data folder - that's where the captured data will go
+mkdir -p -m777 /data
+
+# Install whale tag packages.
+install_package $(ls ${OUT_DIR}/ceti-tag-set-hostname_*_all.deb)
+install_package $(ls ${OUT_DIR}/ceti-tag-data-capture_*_all.deb)
 
 ################################################################################
 #################################### pi user ###################################
@@ -91,4 +132,5 @@ sed -i -e 's/"gb"/"us"/' /etc/default/keyboard
 # Copy filesystem overlay.
 tar -cf - -C "${OVERLAY_DIR}" --owner=pi --group=pi . | tar -xf - -C /
 
+# All done
 echo "( ・◡・)つ━☆   Build complete"
