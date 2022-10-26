0f3c25561f8caad873ff881de906e0328c84061a
EngWorkstation\Matt <matt@cummingselectronics.com>
2022-10-26 00:27:09 -0400

    Add tagMonitor.sh script to the service framework

---
+++ packages/ceti-tag-data-capture/ipc/tagMonitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+
+# Provide a startup delay to allow a user to connect before recorder begins using resources 
+sleep 30
+
+# Launch the main recording application in the background
+sudo ../bin/cetiTagApp & 
+
+
