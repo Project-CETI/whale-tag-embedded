da491b1281cbf66285d44405ccc94c45d354494e
root <matt@cummingselectronics.com>
2022-10-26 20:32:37 -0400

    Add uSD card available space checking

--- packages/ceti-tag-data-capture/ipc/tagMonitor.sh
+++ packages/ceti-tag-data-capture/ipc/tagMonitor.sh
@@ -6,6 +6,28 @@
 sleep 30
 
 # Launch the main recording application in the background
-sudo ../bin/cetiTagApp & 
+sudo /opt/ceti-tag-data-capture/bin/cetiTagApp & 
 
+#----------------------------------------------
+# micro-SD storage monitoring  
 
+while :
+do
+	avail=$(df --output=source,avail | grep /dev/root)
+	echo $avail
+	set $avail
+	echo $2
+
+	if [ $2 -lt 22000000 ]
+  	  then 
+    		echo "disk almost full, stopping the service"
+    		systemctl stop ceti-tag-data-capture
+  	  else 
+    		echo "disk space OK"
+	fi
+
+	sleep 60
+
+done
+
+#----------------------------------------------
