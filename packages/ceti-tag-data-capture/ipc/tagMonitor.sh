ddb427a9443b6179c73609089e8010f4bd62d851
EngWorkstation\Matt <matt@cummingselectronics.com>
2022-11-05 20:24:36 -0400

    Some improvements to the monitor, but still not consistent  - work in progress, still have issues with power down sequence in the case where battery runs out AND the storage is full

--- packages/ceti-tag-data-capture/ipc/tagMonitor.sh
+++ packages/ceti-tag-data-capture/ipc/tagMonitor.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# New for 2.1-4 release  11/3/22
+# New for 2.1-4 release  11/5/22
 
 # Provide a startup delay to allow a user to connect before recorder begins using resources 
-sleep 30
+sleep  30
+
+# Assume there is some storage left on the uSD card
+disk_full=0
 
 # Launch the main recording application in the background
 sudo /opt/ceti-tag-data-capture/bin/cetiTagApp & 
@@ -12,61 +15,74 @@ sudo /opt/ceti-tag-data-capture/bin/cetiTagApp &
 while :
 do
 
-# check that there is disk space available to continue recording	
+# Check that there is disk space available to continue recording	
 	avail=$(df --output=source,avail | grep /dev/root)
 	echo $avail
 	set $avail
 	echo $2
 
 # The tags are normally built with 128 GB uSD cards.  Leave 1 GB headroom
+# Note - sensors.csv will continue, just audio is stopped.  
 
-	if [ $2 -lt 1000000 ]
-  	  then 
-    		echo "disk almost full, stopping the service"
-    		systemctl stop ceti-tag-data-capture
-  	  else 
-    		echo "disk space OK"
+	if [ $2 -lt 1000000 ] &&  [  $disk_full -eq 0 ] 
+	then
+    	echo "disk almost full, suspend audio recording"
+		echo "stopAcq" > cetiCommand
+                sleep 1
+                cat cetiResponse
+                echo "audio recording suspended successfully"
+		disk_full=1
+
+  	 elif  [ $disk_full -eq 1 ] 
+  	 then
+		echo "disk full, recording suspended"
+	 
+	 else
+	 	echo "disk space is OK"
+
 	fi
 
 # check the cell voltages and power off if needed. Hardcoded
 # for now at 3V per cell.  
 
-# use named pipe to grab cell voltages from the recorder app
-	v1=$(./tag.sh checkCell_1)
-	v2=$(./tag.sh checkCell_2)
-	echo "cell voltages $v1 $v2"
+	echo "checkCell_1" > cetiCommand
+    v1=$(cat cetiResponse)
+	echo "checkCell_2" > cetiCommand
+	v2=$(cat cetiResponse)
 
-# If either cell is less than 3.00 V, power down the Pi
-# and signal the FPGA to disable charging and discharging
+	echo "cell voltages are $v1 $v2"
+
+# If either cell is less than 3.00 V:
+#    -signal the FPGA to disable charging and discharging
+#    -schedule Pi shutdown
 
 	check1=$( echo "$v1 < 3.00" | bc )
 	check2=$( echo "$v2 < 3.00" | bc )
 
-	if [ $check1 -gt 0 ] || [ $check2 -gt 0 ]
-	  then
+	if [ $check1 -gt 0 ] || [ $check2 -gt 0 ] 
+	then
 	  	echo "low battery cell detected, powering Pi down now!"
-	  	
-# stopAcq will turn on red LED
-	  	./tag.sh stopAcq	  	
-	  	sleep 1
 
-# powerdown signals FPGA to wait for Pi to turn off
-# then disable charging and discharging. The entire
-# tag will be turned off once the Pi shuts down.
-
-# To subsequently enable the Tag requires connection of
-# a charger and running the tagWake.sh script
-
-	  	./tag.sh powerdown
-	  	sleep 1
-		
-		shutdown -P now   
-
-	  else 
+	  	echo "powerdown" > cetiCommand
+	  	cat cetiResponse
+		shutdown -P +1
+		break	
+	 else 
 	  	echo "battery is OK"
-    fi
+	fi
 
+#loop timing
 	sleep 60
-
 done
+
+# Some clean up on the way out...
+echo "stopping cetiTagApp"
+echo "quit" > cetiCommand
+cat cetiResponse
+
+# Optionally disable the service, must be reenabled when waking the tag up
+# echo "disabling ceti-tag-data-capture service"
+# systemctl disable ceti-tag-data-capture
+
+echo "good night!"
 
