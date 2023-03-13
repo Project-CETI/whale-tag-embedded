e9aef9041081972961a670d89eb3d863ea2779ea
bc-kim <31344331+bc-kim@users.noreply.github.com>
2023-03-13 15:49:06 -0400

    GoPro_Code_update_with_Readmefile

--- packages/ceti-tag-data-capture/src/goPros_interface.py
+++ packages/ceti-tag-data-capture/src/goPros_interface.py
@@ -10,7 +10,12 @@ import select
 import sys
 import time
 from ctypes import *
+import asyncio
+from typing import Optional
+from binascii import hexlify
 
+from bleak import BleakClient
+from tutorial_modules import GOPRO_BASE_UUID, connect_ble, logger
 ####################################################################
 # Define the C interface, which should match the definitions in sensors/goPros.h
 ####################################################################
@@ -24,7 +29,7 @@ class GoPro_C_Message(Structure):
   ]
 
 # Define the commands.
-num_goPros = 4
+num_goPros = 2
 goPro_commands = {
   'start': 1,
   'stop': 2,
@@ -35,79 +40,135 @@ goPro_commands = {
 goPro_c_port = 2300
 connection_heartbeat_period_s = 5
 socket_receive_timeout_s = 5
-
+goPro_status = [0] * num_goPros # used to check whether the goPro is now on or off
 ####################################################################
 # Create the GoPro interface
 ####################################################################
-def control_goPro(goPro_index, command):
-  if command == goPro_commands['start']:
-    print('Starting GoPro %d' % goPro_index)
-  elif command == goPro_commands['stop']:
-    print('Stopping GoPro %d' % goPro_index)
+async def control_goPro(goPro_index, command):
+    # Synchronization event to wait until notification response is received
+    event = asyncio.Event()
+    # UUIDs to write to and receive responses from
+    COMMAND_REQ_UUID = GOPRO_BASE_UUID.format("0072")
+    COMMAND_RSP_UUID = GOPRO_BASE_UUID.format("0073")
+    response_uuid = COMMAND_RSP_UUID
+    #logger.info(goPro_index)
+    
+    if(goPro_index==0):
+        goPro_Name = 6059
+    elif(goPro_index==1):
+        goPro_Name = 7192
+    elif(goPro_index==2): # We have to change it to Camera name as we get them.
+        goPro_Name = 7777    
+    elif(goPro_index==3): # We have to change it to Camera name as we get them.
+        goPro_Name = 7777
+    else:
+        goPro_Name = 7777
+            
+    client: BleakClient
 
+    def notification_handler(handle: int, data: bytes) -> None:
+        logger.info(f'Received response at {handle=}: {hexlify(data, ":")!r}')
+
+        # If this is the correct handle and the status is success, the command was a success
+        if client.services.characteristics[handle].uuid == response_uuid and data[2] == 0x00:
+            logger.info("Command sent successfully")
+        # Anything else is unexpected. This shouldn't happen
+        else:
+            logger.error("Unexpected response")
+
+        # Notify the writer
+        event.set()
+
+    client = await connect_ble(notification_handler, goPro_Name)
+    event.clear()
+    if (command == goPro_commands['start'] and goPro_status[goPro_index]==0):        # Write to command request BleUUID to turn the shutter on
+        logger.info("Setting the shutter on")
+        print('Starting GoPro %d' % goPro_Name)
+        await client.write_gatt_char(COMMAND_REQ_UUID, bytearray([3, 1, 1, 1]), response=True)
+        goPro_status[goPro_index] = 1
+    elif (command == goPro_commands['stop'] and goPro_status[goPro_index]==1):        # Write to command request BleUUID to turn the shutter off
+        logger.info("Setting the shutter off")
+        print('Stopping GoPro %d' % goPro_Name)
+        await client.write_gatt_char(COMMAND_REQ_UUID, bytearray([3, 1, 1, 0]), response=True)
+        goPro_status[goPro_index] = 0
+    await event.wait()  # Wait to receive the notification response
+    await client.disconnect()
+    
 ####################################################################
 # Main loop to listen to and process commands.
 ####################################################################
-def main():
-  while True:
+async def main() -> None:
     # Create the socket.
     print('Creating the socket')
     c_server_address = ('localhost', goPro_c_port)
     c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     c_socket.setblocking(0)
-    
+
     # Repeatedly try to connect to the C server.
     print('Waiting for connection to be accepted')
     is_connected = False
     waiting_start_s = time.time()
     while not is_connected and time.time() - waiting_start_s < 10:
-      try:
-        c_socket.connect(c_server_address)
-        is_connected = True
-        print('Connected to %s' % repr(c_server_address))
-      except AttributeError as ae:
-        pass
-        #print('Error creating the socket: %s' % str(ae))
-      except socket.error as se:
-        pass
-        #print('Exception on socket: %s' % str(se))
-      if not is_connected:
-        time.sleep(0.1)
+        try:
+            c_socket.connect(c_server_address)
+            is_connected = True
+            print('Connected to %s' % repr(c_server_address))
+        except AttributeError as ae:
+            pass
+            #print('Error creating the socket: %s' % str(ae))
+        except socket.error as se:
+            pass
+            #print('Exception on socket: %s' % str(se))
+        if not is_connected:
+            time.sleep(0.1)
     if not is_connected:
-      c_socket.close()
-      time.sleep(1)
-      continue
+        c_socket.close()
+        time.sleep(1)
+        #return
     
     # Continuously wait for and process commands from the C program.
     last_message_time_s = time.time()
     while time.time() - last_message_time_s < 2*connection_heartbeat_period_s:
-      # print('Time since last message: %0.2f' % (time.time() - last_message_time_s))
-      try:
-        data_is_available = select.select([c_socket], [], [], socket_receive_timeout_s)[0]
-        if data_is_available:
-          buff = c_socket.recv(sizeof(GoPro_C_Message))
-          if len(buff) > 0:
-            # Parse the message.
-            c_message = GoPro_C_Message.from_buffer_copy(buff)
-            goPro_index = c_message.goPro_index
-            command = c_message.command
-            print('Received command %d for goPro index %d' % (command, goPro_index))
-            # Process the command.
-            control_goPro(goPro_index, command)
-            # Send an acknowledgment if it is not a heartbeat.
-            if command != goPro_commands['heartbeat']:
-              c_message = GoPro_C_Message(goPro_index, command, 1)
-              c_socket.sendall(c_message)
-            # Update alive time.
-            last_message_time_s = time.time()
-      except socket.error as socket_error:
-        print('Exception on socket: %s' % str(socket_error))
-    
+        # print('Time since last message: %0.2f' % (time.time() - last_message_time_s))
+        try:
+            data_is_available = select.select([c_socket], [], [], socket_receive_timeout_s)[0]
+            if data_is_available:
+                buff = c_socket.recv(sizeof(GoPro_C_Message))
+                if len(buff) > 0:
+                    # Parse the message.
+                    c_message = GoPro_C_Message.from_buffer_copy(buff)
+                    goPro_index = c_message.goPro_index
+                    command = c_message.command
+                    print('Received command %d for goPro index %d' % (command, goPro_index))
+                    # Process the command.
+
+                    # Send an acknowledgment if it is not a heartbeat.
+                    if command != goPro_commands['heartbeat']:
+                        await control_goPro(goPro_index, command)
+                        c_message = GoPro_C_Message(goPro_index, command, 1)
+                        c_socket.sendall(c_message)
+                    # Update alive time.
+                    last_message_time_s = time.time()
+        except socket.error as socket_error:
+            print('Exception on socket: %s' % str(socket_error))
+
     # The C program stopped sending a heartbeat, so close the connection and restart.
     print('No heartbeat! Closing the socket')
     c_socket.close()
     del c_socket
 
+    if(sum(goPro_status)): # If c code stops, it automatically stops the goPro cameras
+        command = 2
+        for x in range(num_goPros):
+            if(goPro_status[x]==1):
+                await control_goPro(x, command)
+                goPro_status[x] = 0
 
 if __name__ == '__main__':
-  main()
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logger.error(e)
+        sys.exit(-1)
+    else:
+        sys.exit(0)
--- packages/ceti-tag-data-capture/src/sensors/goPros.c
+++ packages/ceti-tag-data-capture/src/sensors/goPros.c
@@ -6,7 +6,7 @@
 //-----------------------------------------------------------------------------
 
 #include "goPros.h"
-
+#include <stdlib.h>
 //-----------------------------------------------------------------------------
 // Initialization
 //-----------------------------------------------------------------------------
@@ -20,19 +20,20 @@ goPro_python_message_struct goPro_python_message;
 char goPros_python_buffer[GOPRO_PYTHON_BUFFERSIZE];
 struct sockaddr_in goPros_python_client;
 
-static FILE* goPros_data_file = NULL;
+static FILE *goPros_data_file = NULL;
 static char goPros_data_file_notes[256] = "";
-static const char* goPros_data_file_headers[] = {
-  "GoPro Index",
-  "Command",
-  "Message Ack Time [us]"
-  };
+static const char *goPros_data_file_headers[] = {
+    "GoPro Index",
+    "Command",
+    "Message Ack Time [us]"};
 static const int num_goPros_data_file_headers = 3;
 
-
-int init_goPros() {
+int init_goPros()
+{
   // Initialize the Python communication.
-  if(create_goPros_socket(GOPRO_PYTHON_PORT) < 0)
+  system("python /home/bckim//GoPro/whale/whale-tag-embedded/packages/ceti-tag-data-capture/src/goPros_interface.py &"); // should be changed after deployment
+  usleep(5000000);                                                                                                       // used to wait until python starts
+  if (create_goPros_socket(GOPRO_PYTHON_PORT) < 0)
   {
     CETI_LOG("init_goPros(): XXX ERROR: Socket creation or bind failed XXX");
     return -1;
@@ -40,8 +41,8 @@ int init_goPros() {
   CETI_LOG("init_goPros(): Successfully created the Python socket");
 
   // Open an output file to write data.
-  if(init_data_file(goPros_data_file, GOPROS_DATA_FILEPATH,
-                     goPros_data_file_headers,  num_goPros_data_file_headers,
+  if (init_data_file(goPros_data_file, GOPROS_DATA_FILEPATH,
+                     goPros_data_file_headers, num_goPros_data_file_headers,
                      goPros_data_file_notes, "init_goPros()") < 0)
     return -1;
 
@@ -51,69 +52,71 @@ int init_goPros() {
 //-----------------------------------------------------------------------------
 // Main thread
 //-----------------------------------------------------------------------------
-void* goPros_thread(void* paramPtr) {
-    // Get the thread ID, so the system monitor can check its CPU assignment.
-    g_goPros_thread_tid = gettid();
+void *goPros_thread(void *paramPtr)
+{
+  // Get the thread ID, so the system monitor can check its CPU assignment.
+  g_goPros_thread_tid = gettid();
 
-    // Set the thread CPU affinity.
-    if(GOPROS_CPU >= 0)
-    {
-      pthread_t thread;
-      thread = pthread_self();
-      cpu_set_t cpuset;
-      CPU_ZERO(&cpuset);
-      CPU_SET(GOPROS_CPU, &cpuset);
-      if(pthread_setaffinity_np(thread, sizeof(cpuset), &cpuset) == 0)
-        CETI_LOG("goPros_thread(): Successfully set affinity to CPU %d", GOPROS_CPU);
-      else
-        CETI_LOG("goPros_thread(): XXX Failed to set affinity to CPU %d", GOPROS_CPU);
-    }
+  // Set the thread CPU affinity.
+  if (GOPROS_CPU >= 0)
+  {
+    pthread_t thread;
+    thread = pthread_self();
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(GOPROS_CPU, &cpuset);
+    if (pthread_setaffinity_np(thread, sizeof(cpuset), &cpuset) == 0)
+      CETI_LOG("goPros_thread(): Successfully set affinity to CPU %d", GOPROS_CPU);
+    else
+      CETI_LOG("goPros_thread(): XXX Failed to set affinity to CPU %d", GOPROS_CPU);
+  }
 
-    // Initialize state for socket.
-    int client_accepted = 0;
-    unsigned int goPros_python_client_length = sizeof(goPros_python_client);
+  // Initialize state for socket.
+  int client_accepted = 0;
+  unsigned int goPros_python_client_length = sizeof(goPros_python_client);
 
-    // Accept Python connection request.
-    CETI_LOG("goPros_thread(): Waiting for Python client to accept connection");
-    while(client_accepted == 0 && !g_exit)
-    {
-      goPros_python_connected_socket = accept(goPros_python_socket, (struct sockaddr*)&goPros_python_client, &goPros_python_client_length);
-      client_accepted = (goPros_python_connected_socket >= 0);
-      if(client_accepted == 0)
-        usleep(200000);
-    }
-    CETI_LOG("goPros_thread(): Accepted client connection from Python program (%s)", inet_ntoa(goPros_python_client.sin_addr));
-    bzero(goPros_python_buffer, GOPRO_PYTHON_BUFFERSIZE);
+  // Accept Python connection request.
+  CETI_LOG("goPros_thread(): Waiting for Python client to accept connection");
+  while (client_accepted == 0 && !g_exit)
+  {
+    goPros_python_connected_socket = accept(goPros_python_socket, (struct sockaddr *)&goPros_python_client, &goPros_python_client_length);
+    client_accepted = (goPros_python_connected_socket >= 0);
+    if (client_accepted == 0)
+      usleep(200000);
+  }
+  CETI_LOG("goPros_thread(): Accepted client connection from Python program (%s)", inet_ntoa(goPros_python_client.sin_addr));
+  bzero(goPros_python_buffer, GOPRO_PYTHON_BUFFERSIZE);
 
-    // Start the GoPros!
-    if(!g_exit)
-    {
-      CETI_LOG("goPros_thread(): Starting the GoPros");
-      send_command_to_all_goPros(GOPRO_COMMAND_START);
-    }
+  // Start the GoPros!
+  if (!g_exit)
+  {
+    CETI_LOG("goPros_thread(): Starting the GoPros");
+    send_command_to_all_goPros(GOPRO_COMMAND_START);
+  }
 
-    // Main loop while application is running.
-    CETI_LOG("goPros_thread(): Starting loop to wait while data acquisition runs");
-    g_goPros_thread_is_running = 1;
+  // Main loop while application is running.
+  CETI_LOG("goPros_thread(): Starting loop to wait while data acquisition runs");
+  g_goPros_thread_is_running = 1;
+  usleep(GOPRO_PYTHON_HEARTBEAT_PERIOD_US);
+  while (!g_exit)
+  {
+    // Periodically send a heartbeat to let Python know the connection is active.
+    goPro_python_message.goPro_index = NUM_GOPROS + 1;
+    goPro_python_message.command = GOPRO_COMMAND_HEARTBEAT;
+    goPro_python_message.is_ack = 0;
+    send_goPros_message(&goPro_python_message, sizeof(goPro_python_message_struct));
     usleep(GOPRO_PYTHON_HEARTBEAT_PERIOD_US);
-    while(!g_exit) {
-      // Periodically send a heartbeat to let Python know the connection is active.
-      goPro_python_message.goPro_index = NUM_GOPROS+1;
-      goPro_python_message.command = GOPRO_COMMAND_HEARTBEAT;
-      goPro_python_message.is_ack = 0;
-      send_goPros_message(&goPro_python_message, sizeof(goPro_python_message_struct));
-      usleep(GOPRO_PYTHON_HEARTBEAT_PERIOD_US);
-    }
+  }
 
-    // Stop the GoPros
-    CETI_LOG("goPros_thread(): Stopping the GoPros");
-    send_command_to_all_goPros(GOPRO_COMMAND_STOP);
+  // Stop the GoPros
+  CETI_LOG("goPros_thread(): Stopping the GoPros");
+  send_command_to_all_goPros(GOPRO_COMMAND_STOP);
 
-    // Clean up and exit.
-    close_goPros_socket();
-    g_goPros_thread_is_running = 0;
-    CETI_LOG("goPros_thread(): Done!");
-    return NULL;
+  // Clean up and exit.
+  close_goPros_socket();
+  g_goPros_thread_is_running = 0;
+  CETI_LOG("goPros_thread(): Done!");
+  return NULL;
 }
 
 //-----------------------------------------------------------------------------
@@ -122,33 +125,33 @@ void* goPros_thread(void* paramPtr) {
 
 int create_goPros_socket()
 {
-    if((goPros_python_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        return -1;
+  if ((goPros_python_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return -1;
 
-    bzero((char *) &goPros_python_server, sizeof(goPros_python_server));
-    goPros_python_server.sin_family = AF_INET;
-    goPros_python_server.sin_addr.s_addr = INADDR_ANY;
-    goPros_python_server.sin_port = htons(GOPRO_PYTHON_PORT);
-    if(bind(goPros_python_socket, (struct sockaddr *)&goPros_python_server , sizeof(goPros_python_server)) < 0)
-        return -1;
+  bzero((char *)&goPros_python_server, sizeof(goPros_python_server));
+  goPros_python_server.sin_family = AF_INET;
+  goPros_python_server.sin_addr.s_addr = INADDR_ANY;
+  goPros_python_server.sin_port = htons(GOPRO_PYTHON_PORT);
+  if (bind(goPros_python_socket, (struct sockaddr *)&goPros_python_server, sizeof(goPros_python_server)) < 0)
+    return -1;
 
-    CETI_LOG("create_goPros_socket(): Created socket and completed bind");
+  CETI_LOG("create_goPros_socket(): Created socket and completed bind");
 
-    listen(goPros_python_socket, 3);
-    return goPros_python_socket;
+  listen(goPros_python_socket, 3);
+  return goPros_python_socket;
 }
 
 void close_goPros_socket()
 {
-    close(goPros_python_socket);
-    return;
+  close(goPros_python_socket);
+  return;
 }
 
-int send_goPros_message(void* msg, uint32_t msgsize)
+int send_goPros_message(void *msg, uint32_t msgsize)
 {
-    if(write(goPros_python_connected_socket, msg, msgsize) < 0)
-        return -1;
-    return 0;
+  if (write(goPros_python_connected_socket, msg, msgsize) < 0)
+    return -1;
+  return 0;
 }
 
 void send_command_to_all_goPros(int goPro_command)
@@ -158,14 +161,14 @@ void send_command_to_all_goPros(int goPro_command)
   do
   {
     goPros_data_file = fopen(GOPROS_DATA_FILEPATH, "at");
-    if(goPros_data_file == NULL)
+    if (goPros_data_file == NULL)
     {
       CETI_LOG("send_command_to_all_goPros(): failed to open data output file: %s", GOPROS_DATA_FILEPATH);
       // Sleep a bit before retrying.
-      for(int i = 0; i < 10 && !g_exit; i++)
+      for (int i = 0; i < 10 && !g_exit; i++)
         usleep(500000);
     }
-  } while(goPros_data_file == NULL && !g_exit);
+  } while (goPros_data_file == NULL && !g_exit);
 
   // Initialize state.
   int received_client_ack = 0;
@@ -176,7 +179,7 @@ void send_command_to_all_goPros(int goPro_command)
   int rtc_count_message_sent;
 
   // Send the command to all GoPros via the Python socket.
-  for(int goPro_index = 0; goPro_index < NUM_GOPROS; goPro_index++)
+  for (int goPro_index = 0; goPro_index < NUM_GOPROS; goPro_index++)
   {
     // Send the command.
     goPro_python_message.goPro_index = goPro_index;
@@ -188,15 +191,14 @@ void send_command_to_all_goPros(int goPro_command)
     // Wait for confirmation.
     received_client_ack = 0;
     timeout_start_us = get_global_time_us();
-    while(received_client_ack == 0 && get_global_time_us() - timeout_start_us < 1000000)
+    time_message_ack_us = 0;
+    while (received_client_ack == 0 && get_global_time_us() - timeout_start_us < 1000000) // it means it waits 10sec to get the ack signal -> It should be increased sometime
     {
       num_read_from_client = read(goPros_python_connected_socket, goPros_python_buffer, GOPRO_PYTHON_BUFFERSIZE);
-      if(num_read_from_client > 0)
+      if (num_read_from_client > 0)
       {
-        goPro_python_message = *((goPro_python_message_struct*) goPros_python_buffer);
-        if(goPro_python_message.goPro_index == goPro_index
-            && goPro_python_message.command == goPro_command
-            && goPro_python_message.is_ack == 1)
+        goPro_python_message = *((goPro_python_message_struct *)goPros_python_buffer);
+        if (goPro_python_message.goPro_index == goPro_index && goPro_python_message.command == goPro_command && goPro_python_message.is_ack == 1)
         {
           received_client_ack = 1;
           time_message_ack_us = get_global_time_us();
