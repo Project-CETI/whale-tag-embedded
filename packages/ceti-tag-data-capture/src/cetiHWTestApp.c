58d66693795ff7fad06b20a8baf701ab30a14c49
Michael Salino-Hugg <msalinohugg@seas.harvard.edu>
2024-01-31 13:26:19 -0500

    temperature test implements

--- packages/ceti-tag-data-capture/src/cetiHWTestApp.c
+++ packages/ceti-tag-data-capture/src/cetiHWTestApp.c
@@ -693,6 +693,70 @@ TestState test_pressure(void){
     return (pressure_pass ? TEST_STATE_PASSED : TEST_STATE_FAILED);
 }
 
+TestState test_temperature(void){
+    char input = 0;
+    double garbage;
+    double temp_c[5];
+    bool sensor_pass[sizeof(temp_c)/sizeof(*temp_c)];
+
+    do {
+        //read temperatures
+        int result = 0;
+        result |= getPressureTemperature(&garbage, &temp_c[0]);
+        result |= getTemperatures(&temp_c[1], &temp_c[2]);
+        temp_c[3] = get_cpu_temperature_c();
+        temp_c[4] = get_gpu_temperature_c();
+        if(result != 0){
+            return TEST_STATE_FAILED;
+        }
+
+        // calculate average temperature
+        double average_temp = 0.0;
+        for(int i = 0; i < sizeof(temp_c)/sizeof(*temp_c); i++){
+            average_temp += temp_c[i] / (sizeof(temp_c)/sizeof(*temp_c));
+        }
+
+        //check that temperature reads agree +/- 3 deg
+        for(int i = 0; i < sizeof(temp_c)/sizeof(*temp_c); i++){
+            if(!sensor_pass[i]) {
+                double diff = fabs(average_temp - temp_c[i]);
+                sensor_pass = (diff <= 3.0);
+            }
+        }
+
+
+        //draw results
+        //clear dynamic portion of screen
+        for(int i = 0; i < (1 + sizeof(temp_c)/sizeof(*temp_c)); i++){
+            printf("\e[%d;1H\e[0K", 4 + i);
+        }
+        printf("\e[4;1H");
+        printf("Pressure: %4.1f : %s\n", temp_c[0], sensor_pass[0] ? GREEN(PASS) :  RED(FAIL));
+        printf("Board   : %4.1f : %s\n", temp_c[1], sensor_pass[1] ? GREEN(PASS) :  RED(FAIL));
+        printf("Battery : %4.1f : %s\n", temp_c[2], sensor_pass[2] ? GREEN(PASS) :  RED(FAIL));
+        printf("CPU     : %4.1f : %s\n", temp_c[3], sensor_pass[3] ? GREEN(PASS) :  RED(FAIL));
+        printf("GPU     : %4.1f : %s\n", temp_c[4], sensor_pass[4] ? GREEN(PASS) :  RED(FAIL));
+        printf("Average : %4.1f", average_temp);        
+    } while(read(STDIN_FILENO, &input, 1), input == 0);
+
+    //print results
+    fprintf(results_file, "Pressure: %s\n", sensor_pass[0] ? PASS :  FAIL);
+    fprintf(results_file, "Board   : %s\n", sensor_pass[1] ? PASS :  FAIL);
+    fprintf(results_file, "Battery : %s\n", sensor_pass[2] ? PASS :  FAIL);
+    fprintf(results_file, "CPU     : %s\n", sensor_pass[3] ? PASS :  FAIL);
+    fprintf(results_file, "GPU     : %s\n", sensor_pass[4] ? PASS :  FAIL);
+
+    if(input == 27)
+        return TEST_STATE_TERMINATE;
+
+
+    bool all_passed = 1;
+    for(int i = 0; i < sizeof(sensor_pass)/sizeof(*sensor_pass); i++){
+        all_passed &= sensor_pass[i];
+    }
+    return (all_passed ? TEST_STATE_PASSED : TEST_STATE_FAILED);
+}
+
 
 /************************************
  * CLI control functions 
--- packages/ceti-tag-data-capture/src/sensors/boardTemperature.c
+++ packages/ceti-tag-data-capture/src/sensors/boardTemperature.c
@@ -54,7 +54,7 @@ void* boardTemperature_thread(void* paramPtr) {
       if(pthread_setaffinity_np(thread, sizeof(cpuset), &cpuset) == 0)
         CETI_LOG("Successfully set affinity to CPU %d", BOARDTEMPERATURE_CPU);
       else
-        CETI_LOG("XXX Failed to set affinity to CPU %d", BOARDTEMPERATURE_CPU);
+        CETI_ERR("Failed to set affinity to CPU %d", BOARDTEMPERATURE_CPU);
     }
 
     // Main loop while application is running.
@@ -71,62 +71,62 @@ void* boardTemperature_thread(void* paramPtr) {
         // Sleep a bit before retrying.
         for(int i = 0; i < 10 && !g_stopAcquisition; i++)
           usleep(100000);
+        continue;
       }
-      else
+
+
+      // Acquire timing and sensor information as close together as possible.
+      global_time_us = get_global_time_us();
+      rtc_count = getRtcCount();
+
+      if(getTemperatures(&boardTemperature_c,&batteryTemperature_c) < 0)
+        strcat(boardTemperature_data_file_notes, "ERROR | ");
+
+      if(boardTemperature_c < -80) // it seems to return -83 when no sensor is connected
       {
-        // Acquire timing and sensor information as close together as possible.
-        global_time_us = get_global_time_us();
-        rtc_count = getRtcCount();
-
-        if(getTemperatures(&boardTemperature_c,&batteryTemperature_c) < 0)
-          strcat(boardTemperature_data_file_notes, "ERROR | ");
-
-        if(boardTemperature_c < -80) // it seems to return -83 when no sensor is connected
-        {
-          CETI_LOG("XXX readings are likely invalid");
-          strcat(boardTemperature_data_file_notes, "INVALID? | ");
-        }
-        
-        // ******************   Battery Temperature Checks *************************
-
-        if( (batteryTemperature_c > MAX_CHARGE_TEMP) ||  (batteryTemperature_c < MIN_CHARGE_TEMP) ) {          
-          if (!charging_disabled){  
-            disableCharging();
-            charging_disabled = 1;
-            CETI_LOG("Battery charging disabled, outside thermal limits");
-          }
-        }
-
-        if( (batteryTemperature_c > MAX_DISCHARGE_TEMP) ) {
-         if (!discharging_disabled){  
-            disableDischarging();
-            discharging_disabled = 1;
-            CETI_LOG("Battery discharging disabled, outside thermal limit");
-          }     
-        }
-      
-        // ******************   End Battery Temperature Checks *********************
-        
-        // Write timing information.
-        fprintf(boardTemperature_data_file, "%lld", global_time_us);
-        fprintf(boardTemperature_data_file, ",%d", rtc_count);
-        // Write any notes, then clear them so they are only written once.
-        fprintf(boardTemperature_data_file, ",%s", boardTemperature_data_file_notes);
-        strcpy(boardTemperature_data_file_notes, "");
-        // Write the sensor data.
-        fprintf(boardTemperature_data_file, ",%d", boardTemperature_c);
-        fprintf(boardTemperature_data_file, ",%d", batteryTemperature_c);
-        // Finish the row of data and close the file.
-        fprintf(boardTemperature_data_file, "\n");
-        fclose(boardTemperature_data_file);
-
-        // Delay to implement a desired sampling rate.
-        // Take into account the time it took to acquire/save data.
-        polling_sleep_duration_us = BOARDTEMPERATURE_SAMPLING_PERIOD_US;
-        polling_sleep_duration_us -= get_global_time_us() - global_time_us;
-        if(polling_sleep_duration_us > 0)
-          usleep(polling_sleep_duration_us);
+        CETI_ERR("readings are likely invalid");
+        strcat(boardTemperature_data_file_notes, "INVALID? | ");
       }
+      
+      // ******************   Battery Temperature Checks *************************
+
+      if( (batteryTemperature_c > MAX_CHARGE_TEMP) ||  (batteryTemperature_c < MIN_CHARGE_TEMP) ) {          
+        if (!charging_disabled){  
+          disableCharging();
+          charging_disabled = 1;
+          CETI_LOG("Battery charging disabled, outside thermal limits");
+        }
+      }
+
+      if( (batteryTemperature_c > MAX_DISCHARGE_TEMP) ) {
+        if (!discharging_disabled){  
+          disableDischarging();
+          discharging_disabled = 1;
+          CETI_LOG("Battery discharging disabled, outside thermal limit");
+        }     
+      }
+    
+      // ******************   End Battery Temperature Checks *********************
+      
+      // Write timing information.
+      fprintf(boardTemperature_data_file, "%lld", global_time_us);
+      fprintf(boardTemperature_data_file, ",%d", rtc_count);
+      // Write any notes, then clear them so they are only written once.
+      fprintf(boardTemperature_data_file, ",%s", boardTemperature_data_file_notes);
+      strcpy(boardTemperature_data_file_notes, "");
+      // Write the sensor data.
+      fprintf(boardTemperature_data_file, ",%d", boardTemperature_c);
+      fprintf(boardTemperature_data_file, ",%d", batteryTemperature_c);
+      // Finish the row of data and close the file.
+      fprintf(boardTemperature_data_file, "\n");
+      fclose(boardTemperature_data_file);
+
+      // Delay to implement a desired sampling rate.
+      // Take into account the time it took to acquire/save data.
+      polling_sleep_duration_us = BOARDTEMPERATURE_SAMPLING_PERIOD_US;
+      polling_sleep_duration_us -= get_global_time_us() - global_time_us;
+      if(polling_sleep_duration_us > 0)
+        usleep(polling_sleep_duration_us);
     }
     g_boardTemperature_thread_is_running = 0;
     CETI_LOG("Done!");
