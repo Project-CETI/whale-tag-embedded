e1818a6fd4547130c2a6244779ab04ff21253aa3
shanayabarretto <barretto.shanaya@gmail.com>
2024-03-20 18:34:39 -0400

    started driver files

---
+++ packages/ceti-tag-data-capture/src/max17320.c
@@ -0,0 +1,8 @@
+//-----------------------------------------------------------------------------
+// Project:      CETI Tag Electronics
+// Version:      Refer to _versioning.h
+// Copyright:    Cummings Electronics Labs, Harvard University Wood Lab, MIT CSAIL
+// Contributors: Matt Cummings, Shanaya Barretto, Michael Salino-Hugg
+//-----------------------------------------------------------------------------
+
+#include "max17320.h"
---
+++ packages/ceti-tag-data-capture/src/max17320.h
@@ -0,0 +1,6 @@
+//-----------------------------------------------------------------------------
+// Project:      CETI Tag Electronics
+// Version:      Refer to _versioning.h
+// Copyright:    Cummings Electronics Labs, Harvard University Wood Lab, MIT CSAIL
+// Contributors: Matt Cummings, Shanaya Barretto, Michael Salino-Hugg
+//-----------------------------------------------------------------------------
