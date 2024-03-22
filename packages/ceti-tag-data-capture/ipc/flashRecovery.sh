d2cf49fdcef5e8882f6c786eec85d91edc7d47c8
shanayabarretto <barretto.shanaya@gmail.com>
2024-03-22 16:34:10 -0400

    script edits and removal of flash on boot

--- packages/ceti-tag-data-capture/ipc/flashRecovery.sh
+++ packages/ceti-tag-data-capture/ipc/flashRecovery.sh
@@ -1,4 +1,3 @@
-
 #! /bin/bash
 # To start run ls -lA /dev/serial* and ensure /dev/serial0 -> ttyAMA0
 # Instructions to fix: https://www.youtube.com/watch?v=tCcxFMU1OFE
@@ -9,7 +8,7 @@ sudo /opt/ceti-tag-data-capture/bin/cetiFpgaInit
 code=$?
 if [ $code -ne 0 ]
 then
-        echo 'stopped capture'
+        echo 'Data capture has been halted.'
         sudo systemctl stop ceti-tag-data-capture
         sudo /opt/ceti-tag-data-capture/bin/cetiFpgaInit
 fi
@@ -17,9 +16,9 @@ fi
 # Sets BOOT0 pin high
 x=$(i2cget -y 1 0x38)
 i2cset -y 1 0x38 $((x|0x04))
-echo 'boot high'
+echo 'Boot pin has been set high.'
 
-echo 'reset board'
+echo 'Resetting recovery board...'
 # Resets board
 raspi-gpio set 13 op dl
 sleep 1
@@ -27,42 +26,29 @@ raspi-gpio set 13 dh
 sleep 1
 
 # Checks connection to recovery board
-stm32flash /dev/serial0
+stm32flash /dev/serial0 > /dev/null 2>&1
 code=$?
-counter=0
-
-while [ $counter -le 5 ] && [ $code -eq 1 ]; do
-	echo $counter
-        raspi-gpio set 13 dl
-        sleep 2
-        raspi-gpio set 13 dh
-        sleep 2
-        stm32flash /dev/serial0
-        code=$?
-        counter=$((counter+1))
-done
 
 if [ $code -eq 1 ]
 then
-        echo "stm32flash cannot initialize device."
+        echo 'stm32flash cannot initialize device.'
         exit 1
 fi
 
-echo 'initialized device'
+echo 'stm32 device has been initialized.'
 
-# Download file
-curl -o flash.elf https://github.com/Project-CETI/whale-tag-recovery/blob/d104ac37ef99f0f08ca0998f8e62776b750c8978/KaveetSakshamRecoveryBoard/KaveetSakshamRecoveryBoard.elf
+# Check for elf file existence
+if ! [ -f /opt/ceti-tag-data-capture/ipc/flash.elf ]; then
+        echo "File does not exist. Downloading..."
+        # Download file
+        curl -LJO "https://github.com/Project-CETI/whale-tag-recovery/raw/dev/v2_3/KaveetSakshamRecoveryBoard/KaveetSakshamRecoveryBoard.elf"
+        sudo mv KaveetSakshamRecoveryBoard.elf /opt/ceti-tag-data-capture/ipc/flash.elf
+fi
 
 # Conversion from elf to binary
-elf=${1:-'flash.elf'}
-bin='./flash.bin'
-objcopy -O binary "./${elf}" $bin
-code=$?
-if [ $code -eq 1 ]
-then
-        echo "elf file not found."
-        exit 1
-fi
+elf='/opt/ceti-tag-data-capture/ipc/flash.elf'
+bin='/opt/ceti-tag-data-capture/ipc/flash.bin'
+sudo objcopy -O binary $elf $bin
 
 # Flashes board
 stm32flash -v -w $bin /dev/serial0
@@ -70,16 +56,16 @@ code=$?
 sleep 1
 if [ $code -eq 1 ]
 then
-        echo "Something went wrong when flashing."
+        echo "Something went wrong when flashing stm32."
         exit 1
 fi
-echo 'flash succeeded'
+echo 'Flashing stm32 has succeeded'
 
-echo 'boot low'
 # Sets BOOT0 low to exit bootloader
 i2cset -y 1 0x38 $((x&0xfb))
+echo 'Boot pin has been set low'
 
-echo 'reset'
+echo 'Resetting recovery board...'
 # Resets board
 raspi-gpio set 13 dl
 sleep 1
