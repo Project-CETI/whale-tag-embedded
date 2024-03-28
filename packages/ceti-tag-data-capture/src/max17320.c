ae6129b7f920261af66ac5110d515c880c90efef
shanayabarretto <barretto.shanaya@gmail.com>
2024-03-28 14:36:34 -0400

    more failure logs

--- packages/ceti-tag-data-capture/src/max17320.c
+++ packages/ceti-tag-data-capture/src/max17320.c
@@ -69,6 +69,7 @@ int max17320_clear_write_protection(MAX17320_HandleTypeDef *dev) {
     read = i2cReadWordData(fd, MAX17320_REG_COMM_STAT);
     if (read != CLEARED_WRITE_PROT)
     {
+        CETI_ERR("MAX17320 Clearing write protection failed");
         ret = -1;
     }
     return ret;
