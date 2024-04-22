7b5e645d6b783210ecd5ddf3e589bca5d357dacc
Michael Salino-Hugg <msalinohugg@seas.harvard.edu>
2024-04-22 19:09:27 -0400

    cetiFPGAInit: verbose errors

--- packages/ceti-tag-data-capture/src/cetiFpgaInit.c
+++ packages/ceti-tag-data-capture/src/cetiFpgaInit.c
@@ -1,4 +1,5 @@
 #include <pigpio.h>
+#include <string.h>
 
 #include "fpga.h"
 
@@ -22,6 +23,30 @@ int main(void){
         }
         bytes--;
     }
+    char *filepath = strncat(g_process_path, FPGA_BITSTREAM, sizeof(g_process_path));
 
-    return init_fpga(strncat(g_process_path, FPGA_BITSTREAM, sizeof(g_process_path)));
+    //Initialize fpga
+    ResultFPGA init_result = init_fpga(filepath);
+    switch (init_result){
+        case FPGA_OK:
+            CETI_LOG("FPGA successfully initialized!!!");
+            return 0;
+        
+        case FPGA_ERR_CONFIG_MALLOC:
+            CETI_ERR("Failed to malloc fpga bitstream.");
+            return -1;
+            
+        case FPGA_ERR_BIN_OPEN:
+            CETI_ERR("Failed to open bitstream %s", g_process_path);
+            return -2;
+
+        case FPGA_ERR_N_DONE:
+            CETI_ERR("Did not receive done signal from FPGA.");
+            return -3;
+
+        default:
+            CETI_ERR("Unknown FPGA error.");
+            return -4;
+        
+    }
 }
