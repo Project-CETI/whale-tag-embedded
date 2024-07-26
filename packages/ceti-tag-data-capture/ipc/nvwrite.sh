941ba79a3a2be1a06953149cdb0b248123d9878a
Michael Salino-Hugg <msalinohugg@seas.harvard.edu>
2024-07-26 15:31:35 -0400

    include designcap, rsense and correct iThresholds in nonvolatile memory write

--- packages/ceti-tag-data-capture/ipc/nvwrite.sh
+++ packages/ceti-tag-data-capture/ipc/nvwrite.sh
@@ -7,13 +7,15 @@ i2cset -y 1 0x36 0x61 0x0000 w
 i2cset -y 1 0x36 0x61 0x0000 w
 # 2
 # See BMS NV Write Spreadsheet for explanations
+i2cset -y 1 0x0b 0xcf 0x03e8 w  # MAX17320_REG_NRSENSE
+i2cset -y 1 0x0b 0xb3 0x2710 w  # MAX17320_REG_NDESIGNCAP
 i2cset -y 1 0x0b 0xb5 0xc204 w  # MAX17320_REG_NPACKCFG 
 i2cset -y 1 0x0b 0xb8 0x0830 w  # MAX17320_REG_NNVCFG0 
 i2cset -y 1 0x0b 0xb9 0x2100 w  # MAX17320_REG_NNVCFG1 
 i2cset -y 1 0x0b 0xba 0x822d w  # MAX17320_REG_NNVCFG2 
 i2cset -y 1 0x0b 0xd0 0xa002 w  # MAX17320_REG_NUVPRTTH 
 i2cset -y 1 0x0b 0xd1 0x280a w  # MAX17320_REG_NTPRTTH1 
-i2cset -y 1 0x0b 0xd3 0x01d3 w  # MAX17320_REG_NIPRTTH1 #set new slow current limits for development (2A)
+i2cset -y 1 0x0b 0xd3 0x32ce w  # MAX17320_REG_NIPRTTH1 #set new slow current limits for development (2A)
 i2cset -y 1 0x0b 0xd4 0x0ca0 w  # MAX17320_REG_NBALTH   #disable imbalance charge term for development   
 i2cset -y 1 0x0b 0xd6 0x0313 w  # MAX17320_REG_NPROTMISCTH 
 i2cset -y 1 0x0b 0xd7 0x0c08 w  # MAX17320_REG_NPROTCFG 
@@ -25,6 +27,7 @@ i2cset -y 1 0x0b 0xb0 0x0290 w  # MAX17320_REG_NCONFIG
 i2cset -y 1 0x0b 0xca 0x71be w  # MAX17320_REG_NTHERMCFG 
 i2cset -y 1 0x0b 0x9e 0x9659 w  # MAX17320_REG_NVEMPTY 
 i2cset -y 1 0x0b 0xc6 0x5005 w  # MAX17320_REG_NFULLSOCTHR 
+
 # 3
 i2cset -y 1 0x36 0x61 0x0000 w
 # 4
@@ -33,7 +36,7 @@ i2cset -y 1 0x36 0x60 0xE904 w
 sleep 10
 # 6
 x=$(i2cget -y 1 0x36 0x61 w)
-y=$(x&0x0004)
+y=$((x&0x0004))
 if [ $y -eq 0x0004 ]
 then
     echo 'NV Error Bit not cleared, try again'
@@ -51,7 +54,7 @@ i2cset -y 1 0x36 0xAB 0x8000 w
 # 11
 sleep 1
 x=$(i2cget -y 1 0x36 0xAB w)
-y=$(x&0x8000)
+y=$((x&0x8000))
 if [ $y -eq 0x0008 ]
 then
     echo 'POR Process not complete. Wait, then clear write protection'
