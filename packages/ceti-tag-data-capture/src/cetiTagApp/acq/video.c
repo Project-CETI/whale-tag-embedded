be88f8adf183d4e03b25b81e58a8a0cd2e1d9886
Michael Salino-Hugg <msalinohugg@seas.harvard.edu>
2025-04-21 17:30:45 -0400

    reduce libcamera verbosity, update video save filepath

--- packages/ceti-tag-data-capture/src/cetiTagApp/acq/video.c
+++ packages/ceti-tag-data-capture/src/cetiTagApp/acq/video.c
@@ -16,8 +16,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #define VIDEO_FILE_LENGTH_MS 300000
 #define xstr(s) stringify(s)
@@ -28,16 +28,20 @@ int g_video_thread_is_running = 0;
 void *video_thread(void *paramPtr) {
     g_video_thread_tid = gettid();
 
-    pid_t pid;    
+    pid_t pid;
     int64_t video_start_time_us = get_global_time_us();
-    pid=fork();
-    if (pid==0){
+    pid = fork();
+    if (pid == 0) {
         // the spawned child runs this code
-        char video_filename[256] = ""; 
-        sprintf(video_filename, "%ld.h264", get_global_time_us());
+        char video_filename[256] = "";
+        sprintf(video_filename, "/data/%ld.h264", get_global_time_us());
         char *argv[] = {
-            "libcamera-vid", "-t",  xstr(VIDEO_FILE_LENGTH_MS), "--inline", "-o", video_filename, NULL
-        };                
+            "libcamera-vid",
+            "-t", xstr(VIDEO_FILE_LENGTH_MS),
+            "--inline",
+            "-v", "=0",
+            "-o", video_filename,
+            NULL};
         execvp("libcamera-vid", argv);
         // we only get here if the child failed
         CETI_ERR("failed to created libcamera-vid child process");
@@ -47,42 +51,40 @@ void *video_thread(void *paramPtr) {
 
     while (!g_stopAcquisition) {
         int64_t timestamp_us = get_global_time_us();
-        
-        //start a new video
+
+        // start a new video
         if ((timestamp_us - video_start_time_us) >= (VIDEO_FILE_LENGTH_MS * 1000)) {
-            //wait for old pid to finish
-            waitpid(pid, NULL, 0);  // Wait for child to finish
-  
-            pid=fork();
+            // wait for old pid to finish
+            waitpid(pid, NULL, 0); // Wait for child to finish
+
+            pid = fork();
             video_start_time_us = get_global_time_us();
-            if (pid==0){
+            if (pid == 0) {
                 // the spawned child process runs this code
-                char video_filename[256] = ""; 
+                char video_filename[256] = "";
                 sprintf(video_filename, "%ld.h264", get_global_time_us());
                 char *argv[] = {
-                    "libcamera-vid", "-t",  xstr(VIDEO_FILE_LENGTH_MS), "--inline", "-o", video_filename, NULL
-                };                
+                    "libcamera-vid", "-t", xstr(VIDEO_FILE_LENGTH_MS), "--inline", "-o", video_filename, NULL};
                 execvp("libcamera-vid", argv);
                 // we only get here if the child failed
                 CETI_ERR("failed to created libcamera-vid child process");
                 exit(1);
             }
         }
-            
-        
-        //sleep a bit
+
+        // sleep a bit
         int64_t elapsed_time_us = get_global_time_us() - timestamp_us;
         int64_t sleep_time_us = 1000000 - elapsed_time_us;
         if (sleep_time_us > 0) {
             usleep(sleep_time_us);
         }
-    }  
+    }
 
-    //kill video process
+    // kill video process
     printf("Stopping recording early...\n");
     kill(pid, SIGINT);
 
-    waitpid(pid, NULL, 0);  // Wait for child to finish
+    waitpid(pid, NULL, 0); // Wait for child to finish
     g_video_thread_is_running = 0;
     return NULL;
 }
