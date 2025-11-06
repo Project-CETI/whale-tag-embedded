40b6d48423b9d0678ae7785a1f8f7c9d5c7b1221
Michael Salino-Hugg <msalinohugg@seas.harvard.edu>
2025-11-06 15:51:56 -0500

    debug ecg crash

--- packages/ceti-tag-data-capture/src/cetiTagApp/sensors/ecg.c
+++ packages/ceti-tag-data-capture/src/cetiTagApp/sensors/ecg.c
@@ -306,6 +306,9 @@ void *ecg_thread_getData(void *paramPtr) {
              1000.0 * (float)sample_index / (float)duration_ms,
              sample_index, duration_ms);
 
+    // wait for ecg writing thread to stop before freeing up resources
+    threadManager_join_thread(THREAD_ECG_LOG);
+
     // Clean up.
     ecg_adc_cleanup();
     munmap(shm_ecg, sizeof(CetiEcgBuffer));
